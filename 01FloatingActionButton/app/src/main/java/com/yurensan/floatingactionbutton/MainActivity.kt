41ec1d96b0175049002a0c212ecb92c35b5ca453
yurensan <yurensan@163.com>
2020-12-22 23:43:35 +0800

    Add FloatingActionButton and Snackbar with action

--- 01FloatingActionButton/app/src/main/java/com/yurensan/floatingactionbutton/MainActivity.kt
+++ 01FloatingActionButton/app/src/main/java/com/yurensan/floatingactionbutton/MainActivity.kt
@@ -2,10 +2,22 @@ package com.yurensan.floatingactionbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { v: View ->
+            Snackbar.make(v, "I am Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("retry") {
+                    Toast.makeText(this, "click me", Toast.LENGTH_LONG).show()
+                }.show()
+        }
     }
 }
