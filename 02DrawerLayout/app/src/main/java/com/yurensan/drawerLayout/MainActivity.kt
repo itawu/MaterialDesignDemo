8608a3b3cd56878645df3fdecfc6b76cb40ebffd
yurensan <yurensan@163.com>
2021-11-03 07:52:39 +0800

    update 02DrawerLayout

--- 02DrawerLayout/app/src/main/java/com/yurensan/drawerLayout/MainActivity.kt
+++ 02DrawerLayout/app/src/main/java/com/yurensan/drawerLayout/MainActivity.kt
@@ -1,37 +1,33 @@
 package com.yurensan.drawerLayout
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mToolbar: Toolbar
-    private lateinit var mDrawerLayout: DrawerLayout
+    private var mDrawerToggle: ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mToolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(mToolbar)
-        mDrawerLayout = findViewById(R.id.drawer_layout)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val drawerList = findViewById<ListView>(R.id.left_drawer)
+        drawerList.adapter = ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, arrayOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"))
 
-        val actionBar: ActionBar? = supportActionBar
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true) //让导航按钮显示出来
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu) //设置一个导航按钮图标
-        }
-    }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            mDrawerLayout.openDrawer(GravityCompat.START)
-        }
-        return true
+        mDrawerToggle = ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close)
+        mDrawerToggle!!.syncState()
+        drawerLayout.addDrawerListener(mDrawerToggle!!)
     }
 }
